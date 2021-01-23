973cb4f8923ce6b6867fabada8cc8c56ac8adf21
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-01-23 09:49:14 +0900

    アイキャッチ作成

--- www/html/sharehouse/resources/views/welcome.blade.php
+++ www/html/sharehouse/resources/views/welcome.blade.php
@@ -2,5 +2,11 @@
 @section('title','トップページ')
 @section('contents')
 
+<div id='eyechach'>
+    <img src="images/eyecatch_room.jpg" alt="部屋の背景" class='eyecatch-img'>
+    <p class='description'>東京23区のシェアハウスを探そう</p>
+    <div class='search'>
+    </div>
+</div>
 
 @endsection
