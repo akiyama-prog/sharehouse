aad9751c54ea799a82d4885d9f0e540c08dfaf92
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-02-02 22:54:22 +0900

    テストデータ作成(seeder)

--- www/html/sharehouse/database/seeds/DatabaseSeeder.php
+++ www/html/sharehouse/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            AreaTableSeeder::class,
+            PropertySeeder::class,
+            RoomsSeeder::class,
+            SubPictureSeeder::class,
+        ]);
     }
 }
