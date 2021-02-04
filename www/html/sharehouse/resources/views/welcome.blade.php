417f0d48d1e921e58d88726d6333df8cf1c8130f
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-02-04 23:56:31 +0900

    エリア検索の実装

--- www/html/sharehouse/resources/views/welcome.blade.php
+++ www/html/sharehouse/resources/views/welcome.blade.php
@@ -4,9 +4,18 @@
 
 <div id='eyechach'>
     <img src="images/eyecatch_room.jpg" alt="部屋の背景" class='eyecatch-img'>
-    <p class='description'>東京23区のシェアハウスを探そう</p>
+    <h2 class='description'>東京23区のシェアハウスを探そう</h2>
     <div class='searches'>
-        <div class='search'>test</div>
+        <div class='search'>
+            <p>エリアで探す</p>
+            <table class='table table-hover'>
+                @foreach($areas as $area)
+                <tr>
+                    <td>{!! Form::submit($area->area_name,['value' => $area->id,'class'=>'area-select']) !!}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
         <div class='search'>test</div>
     </div>
 </div>
