039801f30f04c1e4de825c46c190f4b0fe35b47c
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-02-10 22:42:25 +0900

    ページ内リンク作成

--- www/html/sharehouse/resources/views/property/show.blade.php
+++ www/html/sharehouse/resources/views/property/show.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.layouts')
 @section('title',$property->property_name)
 @section('contents')
-<div class='main-info'>
-    <h1>{{ $property->property_name }}</h1>
-    <div class='main-image'><img src="/images/{{ $property->main_image }}" alt="物件写真"></div>
-    <p class='introduce'>{{ nl2br($property->introduce) }}</p>
+<div id='property-show'>
+    <div class='main-info'>
+        <h1>{{ $property->property_name }}</h1>
+        <div class='main-image'><img src="/images/{{ $property->main_image }}" alt="物件写真"></div>
+        <p class='introduce'>{{ nl2br($property->introduce) }}</p>
+    </div>
+    <ul class='inside-page-link'>
+        <a href="">
+            <li>FACILITIES</br><span class='jp'>共用部</span></br><span class='facilities-arrow'><i class="fas fa-chevron-down"></i></span></li>
+        </a>
+        <a href="">
+            <li>ROOMS</br><span class='jp'>占有部</span></br><span class='rooms-arrow'><i class="fas fa-chevron-down"></i></span></li>
+        </a>
+        <a href="">
+            <li>LOCATION</br><span class='jp'>アクセス・環境</span></br><span class='location-arrow'><i class="fas fa-chevron-down"></i></span></li>
+        </a>
+        <a href="">
+            <li>OPARATIOM</br><span class='jp'>運営・管理</span></br><span class='oparation-arrow'><i class="fas fa-chevron-down"></i></span></li>
+        </a>
+    </ul>
+
 </div>
 @endsection
