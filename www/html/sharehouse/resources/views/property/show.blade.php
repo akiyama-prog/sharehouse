bb60e7d28d886fa34385a2c2055d8ff716d2b574
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-02-11 14:29:48 +0900

    共用部タイトル作成

--- www/html/sharehouse/resources/views/property/show.blade.php
+++ www/html/sharehouse/resources/views/property/show.blade.php
@@ -7,9 +7,10 @@
         <div class='main-image'><img src="/images/{{ $property->main_image }}" alt="物件写真"></div>
         <p class='introduce'>{{ nl2br($property->introduce) }}</p>
     </div>
+
     <ul class='inside-page-link'>
-        <a href="">
-            <li>FACILITIES</br><span class='jp'>共用部</span></br><span class='facilities-arrow'><i class="fas fa-chevron-down"></i></span></li>
+        <a href="#facilities">
+            <li>FACILITIES</br><span class='jp'>共用部</span></br><span class='facilities-color'><i class="fas fa-chevron-down"></i></span></li>
         </a>
         <a href="">
             <li>ROOMS</br><span class='jp'>占有部</span></br><span class='rooms-arrow'><i class="fas fa-chevron-down"></i></span></li>
@@ -21,6 +22,22 @@
             <li>OPARATIOM</br><span class='jp'>運営・管理</span></br><span class='oparation-arrow'><i class="fas fa-chevron-down"></i></span></li>
         </a>
     </ul>
+
+    <div class='facilities'>
+        <div class='facilities-title'>
+            <a id="facilities">
+                <h2><span class='facilities-color'>F</span>ACILITIES</br><span class='jp'>共用部</span></h2>
+            </a>
+            @if($property->feature1)
+            <ul class='feature'>
+                <p class='featurue-title'>FEATURE</p>
+                <li>{{ $property->feature1 }}</li>
+                @if($property->feature2)<li>{{ $property->feature2 }}</li>@endif
+                @if($property->feature3)<li>{{ $property->feature3 }}</li>@endif
+            </ul>
+            @endif
+        </div>
+    </div>
 
 </div>
 @endsection
